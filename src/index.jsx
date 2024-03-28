1f3dc629d1d27b04b161e185f5431652d45d498b
[Github <dudwnsv123@naver.com>
2024-03-28 17:27:52 +0900

    React-test

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './apps/App07_users';
+import App from './apps/index.html';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
