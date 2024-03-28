c1d7e09ea27c11226f1b89b46b2fc2da75934a80
[Github <dudwnsv123@naver.com>
2024-03-28 12:47:45 +0900

    React

--- src/apps/App05_input.jsx
+++ src/apps/App05_input.jsx
@@ -1,23 +1,14 @@
-import { useState } from 'react';
 import './App.css';
+import CardInput from '../components/CardInput';
+import CardInput2 from '../components/CardInput2';
 
 function App() {
-  const [name, setName] = useState('제임스');
-  const [content, setContent] = useState('');
   return (
-    <div className="card">    
-      <div style={{fontSize: '5rem'}}>{name}</div>
-      <h3>{content}</h3>
+    <>
+      <CardInput name={'제임스'} />
       <br />
-      <button style={{padding:'5px', margin:'5px'}} onClick={() => {
-        const newName = prompt('이름을 입력하세요.');
-        setName(newName);
-      }}>이름 바꾸기</button>
-      <br />
-      <input style={{padding:'5px', margin:'5px'}} type="text" onChange={event => {
-        setContent(event.target.value);
-      }} />
-    </div>
+      <CardInput2 name={'마리아'} email={'maria@gmail.com'} />
+    </>
   );
 }
 
