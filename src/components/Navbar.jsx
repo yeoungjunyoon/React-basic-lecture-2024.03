13669130621589c6adfb6cd6cfc810c359c8f333
[Github <dudwnsv123@naver.com>
2024-04-03 09:21:59 +0900

    React router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav>
-      <Link to='/' style={{margin     }}>Home</Link>
+      <Link to='/' style={{marginRight: '10px'}}>Home</Link>
       <Link to='/board'>Board</Link>
     </nav>
   );
